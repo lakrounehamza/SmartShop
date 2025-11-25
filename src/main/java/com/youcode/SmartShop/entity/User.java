0e3acb14e1737ada671f061df08d27f0529a8497
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-25 15:28:31 +0100

    feat(entity) :  add  user entity

---
+++ src/main/java/com/youcode/SmartShop/entity/User.java
@@ -0,0 +1,25 @@
+package com.youcode.SmartShop.entity;
+
+import com.youcode.SmartShop.enums.UserRole;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+    private UserRole role;
+}
