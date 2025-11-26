fe1b93e20072366c34746f951d658822f2199880
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-26 11:13:39 +0100

    feat(entity) :  add  paiement  entity

---
+++ src/main/java/com/youcode/SmartShop/entity/Paiement.java
@@ -0,0 +1,29 @@
+package com.youcode.SmartShop.entity;
+
+import com.youcode.SmartShop.enums.PaymentType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class Paiement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id   ;
+    private   long  id_commande ;
+    private   String numero_paiement;
+    private BigDecimal montant ;
+    private PaymentType type_paiement ;
+    private LocalDate  date_paiement ;
+    private LocalDate  date_encaissement ;
+    @ManyToOne
+    @JoinColumn(name = "commande_id" ,nullable = false)
+    private Commande commande;
+}
