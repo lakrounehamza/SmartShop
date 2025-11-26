bcc21d6ec85cd5f0e3510b9f31b773baff71ba3f
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-26 13:54:11 +0100

    feat(dto) :  add dot responce  for  Produit  entity

---
+++ src/main/java/com/youcode/SmartShop/dtos/response/ProduitRepositoryDto.java
@@ -0,0 +1,10 @@
+package com.youcode.SmartShop.dtos.response;
+
+import java.math.BigDecimal;
+
+public class ProduitRepositoryDto {
+    private Long id;
+    private String nom;
+    private BigDecimal prix;
+    private int stock;
+}
