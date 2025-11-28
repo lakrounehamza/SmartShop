9a35ebff63a65b9fe45da06b1dfdf219990f7287
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-28 11:27:23 +0100

    fix(DTO): correct Command request DTO

--- src/main/java/com/youcode/SmartShop/dtos/request/CommandeCreateRequestDto.java
+++ src/main/java/com/youcode/SmartShop/dtos/request/CommandeCreateRequestDto.java
@@ -19,24 +19,5 @@ public record CommandeCreateRequestDto(
         long client_id,
         @NotNull(message = "La commande doit contenir au minimum un article.")
         List<OrderItem> aticles,
-        @NotNull(message = "La liste des paiements ne peut pas etre vide.")
-        List<Paiement> paiement,
-        @NotNull(message = "La date de la commande ne peut pas etre nulle.")
-        LocalDate date,
-        @DecimalMin(value = "0.0", inclusive = false, message = "Le sous-total doit etre superieur à zero.")
-        @Positive(message = "Le sous-total doit etre un montant positif.")
-        BigDecimal sousTotal,
-        @Positive(message = "La remise doit etre un nombre positif.")
-        int remise,
-        @Positive(message = "Le taux de TVA doit etre un nombre positif.")
-        int TVA,
-        @DecimalMin(value = "0.0", inclusive = false, message = "Le montant total doit etre superieur à zero.")
-        @Positive(message = "Le montant total doit etre positif.")
-        BigDecimal total,
-        String codePromo,
-        @NotNull(message = "Le statut de la commande est obligatoire.")
-        OrderStatus statut,
-        @DecimalMin(value = "0.0", inclusive = false, message = "Le montant restant doit etre superieur e zero.")
-        @Positive(message = "Le montant restant doit etre positif.")
-        BigDecimal montant_restant
+        String codePromo
 ) {}
