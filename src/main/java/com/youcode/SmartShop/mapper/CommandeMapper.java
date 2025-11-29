e4639ff726dd220cc57ea0f1364348dd1621b3d6
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-29 11:41:32 +0100

    fix(mapper) : fix mapping bet ween client and client_id  for (entity and DTO)  commande

--- src/main/java/com/youcode/SmartShop/mapper/CommandeMapper.java
+++ src/main/java/com/youcode/SmartShop/mapper/CommandeMapper.java
@@ -2,11 +2,20 @@ package com.youcode.SmartShop.mapper;
 
 import com.youcode.SmartShop.dtos.request.CommandeCreateRequestDto;
 import com.youcode.SmartShop.dtos.response.CommandeResponseDto;
+import com.youcode.SmartShop.entity.Client;
 import com.youcode.SmartShop.entity.Commande;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CommandeMapper {
+    @Mapping(target = "client", expression = "java(mapClient(request.client_id()))")
     Commande toEntity(CommandeCreateRequestDto request);
+    @Mapping(target = "client_id", source = "client.id")
     CommandeResponseDto toDTO(Commande entity);
+    default Client mapClient(long id) {
+        Client c = new Client();
+        c.setId(id);
+        return c;
+    }
 }
