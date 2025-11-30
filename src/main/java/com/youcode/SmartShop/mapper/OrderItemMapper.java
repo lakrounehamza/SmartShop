fb043994b12b304e1588b5ebc3cd45c7f33a9db1
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-30 14:10:51 +0100

    fix(mapper) : fix mapper form order item add target

--- src/main/java/com/youcode/SmartShop/mapper/OrderItemMapper.java
+++ src/main/java/com/youcode/SmartShop/mapper/OrderItemMapper.java
@@ -3,10 +3,20 @@ package com.youcode.SmartShop.mapper;
 import com.youcode.SmartShop.dtos.request.OrderItemCreateRequestDto;
 import com.youcode.SmartShop.dtos.response.OrderItemResponseDto;
 import com.youcode.SmartShop.entity.OrderItem;
+import com.youcode.SmartShop.entity.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface OrderItemMapper {
-    OrderItem  toEntity(OrderItemCreateRequestDto request);
-    OrderItemResponseDto toDTO(OrderItem entity);
+
+    @Mapping(source = "produit_id", target = "product")
+    OrderItem toEntity(OrderItemCreateRequestDto request);
+        OrderItemResponseDto toDTO(OrderItem entity);
+        default Product fromId(Long id) {
+        if (id == null) return null;
+        Product p = new Product();
+        p.setId(id);
+        return p;
+    }
 }
