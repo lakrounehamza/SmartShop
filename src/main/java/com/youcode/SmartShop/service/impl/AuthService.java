b78213db64fdaf1c434b4ec78fa66b85a1ce5f73
lakrounehamza <lakrounehamza10@gmail.com>
2025-12-03 13:30:41 +0100

    feat(service) : add authService

---
+++ src/main/java/com/youcode/SmartShop/service/impl/AuthService.java
@@ -0,0 +1,45 @@
+package com.youcode.SmartShop.service.impl;
+
+import com.youcode.SmartShop.dtos.request.LoginRequestDto;
+import com.youcode.SmartShop.dtos.response.LoginResponseDto;
+import com.youcode.SmartShop.entity.User;
+import com.youcode.SmartShop.exception.IncorrectPasswordException;
+import com.youcode.SmartShop.exception.NotFoundException;
+import com.youcode.SmartShop.repository.UserRepository;
+import com.youcode.SmartShop.service.interfaces.IAuthService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+@Service
+@RequiredArgsConstructor
+public class AuthService implements IAuthService {
+
+    private final HttpSession session;
+    private final UserRepository userRepository;
+
+    @Override
+    public LoginResponseDto login(LoginRequestDto request) {
+
+        User user = userRepository.findByUsername(request.username())
+                .orElseThrow(() -> new NotFoundException("username incorrect."));
+        if (!BCrypt.checkpw(request.password(), user.getPassword())) {
+            throw new IncorrectPasswordException("password incorrect.");
+        }
+
+        session.setAttribute("id", user.getId());
+        session.setAttribute("role", user.getRole());
+
+        return new LoginResponseDto(
+                "Connexion reussie",
+                user.getId(),
+                user.getUsername(),
+                user.getRole().name()
+        );
+    }
+
+    @Override
+    public void logout() {
+        session.invalidate();
+    }
+}
---
+++ src/main/java/com/youcode/SmartShop/service/interfaces/IAuthService.java
@@ -0,0 +1,9 @@
+package com.youcode.SmartShop.service.interfaces;
+
+import com.youcode.SmartShop.dtos.request.LoginRequestDto;
+import com.youcode.SmartShop.dtos.response.LoginResponseDto;
+
+public interface IAuthService {
+    LoginResponseDto login(LoginRequestDto request);
+    void logout();
+}
