fffa0f9b8e8051ad830e6a56591c397dd950f13a
lakrounehamza <lakrounehamza10@gmail.com>
2025-12-03 12:45:35 +0100

    fix(service) : fix  saved cheque

--- src/main/java/com/youcode/SmartShop/service/impl/ChequeServiceImpl.java
+++ src/main/java/com/youcode/SmartShop/service/impl/ChequeServiceImpl.java
@@ -3,15 +3,20 @@ package com.youcode.SmartShop.service.impl;
 import com.youcode.SmartShop.dtos.request.ChequeCreateRequestDto;
 import com.youcode.SmartShop.dtos.response.ChequeResponseDto;
 import com.youcode.SmartShop.entity.Cheque;
+import com.youcode.SmartShop.entity.Commande;
+import com.youcode.SmartShop.enums.OrderStatus;
+import com.youcode.SmartShop.exception.IncorrectInputException;
 import com.youcode.SmartShop.exception.NotFoundException;
 import com.youcode.SmartShop.mapper.ChequeMapper;
 import com.youcode.SmartShop.repository.ChequeRepository;
+import com.youcode.SmartShop.repository.CommandeRepository;
 import com.youcode.SmartShop.service.interfaces.IChequeService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 
 
 @Service
@@ -20,10 +25,25 @@ public class ChequeServiceImpl implements IChequeService {
 
     private final ChequeRepository repository;
     private final ChequeMapper mapper;
+    private  final CommandeRepository commandeRepository;
 
     @Override
-    public ChequeResponseDto save(ChequeCreateRequestDto request) {
+    public ChequeResponseDto save(ChequeCreateRequestDto request)  {
         Cheque cheque = mapper.toEntity(request);
+
+        Commande commande = commandeRepository.findById(request.commande_id())
+                .orElseThrow(() -> new NotFoundException("Commande introuvable"));
+        BigDecimal res = commande.getMontant_restant()
+                .subtract(request.montant());
+        if (res.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IncorrectInputException("Montant paye depasse le montant restant" +commande.getMontant_restant());
+        }
+        commande.setMontant_restant(res);
+        if (res.compareTo(BigDecimal.ZERO) == 0) {
+            commande.setStatut(OrderStatus.CANCELED);
+        }
+        commandeRepository.save(commande);
+//        cheque.setCommande(commande);
         Cheque chequeSaved = repository.save(cheque);
         return mapper.toDTO(chequeSaved);
     }
