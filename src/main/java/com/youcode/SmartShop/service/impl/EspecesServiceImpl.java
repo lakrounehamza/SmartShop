1c13e0a5f92c2d2648284536b78e50db53168d02
lakrounehamza <lakrounehamza10@gmail.com>
2025-12-03 12:47:13 +0100

    fix(service) : fix  saved cheque and  virement

--- src/main/java/com/youcode/SmartShop/service/impl/EspecesServiceImpl.java
+++ src/main/java/com/youcode/SmartShop/service/impl/EspecesServiceImpl.java
@@ -2,9 +2,13 @@ package com.youcode.SmartShop.service.impl;
 
 import com.youcode.SmartShop.dtos.request.EspecesCreateRequestDto;
 import com.youcode.SmartShop.dtos.response.EspecesResponseDto;
+import com.youcode.SmartShop.entity.Commande;
 import com.youcode.SmartShop.entity.Especes;
+import com.youcode.SmartShop.enums.OrderStatus;
+import com.youcode.SmartShop.exception.IncorrectInputException;
 import com.youcode.SmartShop.exception.NotFoundException;
 import com.youcode.SmartShop.mapper.EspecesMapper;
+import com.youcode.SmartShop.repository.CommandeRepository;
 import com.youcode.SmartShop.repository.EspecesRepository;
 import com.youcode.SmartShop.service.interfaces.IEspecesService;
 import lombok.AllArgsConstructor;
@@ -12,18 +16,33 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
 @AllArgsConstructor
 public class EspecesServiceImpl implements IEspecesService {
 
     private final EspecesMapper especesMapper;
     private final EspecesRepository especesRepository;
+    private final CommandeRepository commandeRepository;
 
     @Override
     public EspecesResponseDto save(EspecesCreateRequestDto request) {
         Especes especes = especesMapper.toEntity(request);
+        Commande commande = commandeRepository.findById(request.commande_id())
+                .orElseThrow(() -> new NotFoundException("Commande introuvable"));
+        BigDecimal res = commande.getMontant_restant()
+                .subtract(request.montant());
+        if (res.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IncorrectInputException("Montant paye depasse le montant restant" +commande.getMontant_restant());
+        }
+        commande.setMontant_restant(res);
+        if (res.compareTo(BigDecimal.ZERO) == 0) {
+            commande.setStatut(OrderStatus.CANCELED);
+        }
+        commandeRepository.save(commande);
         Especes especesSaved = especesRepository.save(especes);
-        return especesMapper.toDTO(especesSaved);
+        return especesMapper.toDTO(especes);
     }
 
     @Override
--- src/main/java/com/youcode/SmartShop/service/impl/VirementServiceImpl.java
+++ src/main/java/com/youcode/SmartShop/service/impl/VirementServiceImpl.java
@@ -2,9 +2,13 @@ package com.youcode.SmartShop.service.impl;
 
 import com.youcode.SmartShop.dtos.request.VirementCreateRequestDto;
 import com.youcode.SmartShop.dtos.response.VirementResponseDto;
+import com.youcode.SmartShop.entity.Commande;
 import com.youcode.SmartShop.entity.Virement;
+import com.youcode.SmartShop.enums.OrderStatus;
+import com.youcode.SmartShop.exception.IncorrectInputException;
 import com.youcode.SmartShop.exception.NotFoundException;
 import com.youcode.SmartShop.mapper.VirementMapper;
+import com.youcode.SmartShop.repository.CommandeRepository;
 import com.youcode.SmartShop.repository.VirementRepository;
 import com.youcode.SmartShop.service.interfaces.IVirementService;
 import lombok.AllArgsConstructor;
@@ -12,15 +16,30 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
 @AllArgsConstructor
 public class VirementServiceImpl implements IVirementService {
     private final VirementRepository  repository;
     private final VirementMapper  mapper;
+    private final CommandeRepository commandeRepository;
     @Override
     public VirementResponseDto save(VirementCreateRequestDto request) {
         Virement virement  = mapper.toEntity(request);
         Virement virementSaved = repository.save(virement);
+        Commande commande = commandeRepository.findById(request.commande_id())
+                .orElseThrow(() -> new NotFoundException("Commande introuvable"));
+        BigDecimal res = commande.getMontant_restant()
+                .subtract(request.montant());
+        if (res.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IncorrectInputException("Montant paye depasse le montant restant" +commande.getMontant_restant());
+        }
+        commande.setMontant_restant(res);
+        if (res.compareTo(BigDecimal.ZERO) == 0) {
+            commande.setStatut(OrderStatus.CANCELED);
+        }
+        commandeRepository.save(commande);
         return mapper.toDTO(virementSaved);
     }
 
