5fb2c256f09f1116d14054dd066509e19c538522
lakrounehamza <lakrounehamza10@gmail.com>
2025-12-07 22:16:57 +0100

    fix(DTO): add validation du code promo dans CommandeCreateRequestDTO

--- src/main/java/com/youcode/SmartShop/dtos/request/CommandeCreateRequestDto.java
+++ src/main/java/com/youcode/SmartShop/dtos/request/CommandeCreateRequestDto.java
@@ -2,6 +2,7 @@ package com.youcode.SmartShop.dtos.request;
 
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 
@@ -10,5 +11,9 @@ public record CommandeCreateRequestDto(
         @NotNull(message = "L'ID du client ne peut pas etre nul.")
         @Positive(message = "L'ID du client ne doit pas etre negatif.")
         long client_id,
+        @Pattern(
+                regexp = "^PROMO-\\d{4}$",
+                message = "Le code promo doit respecter le format 'PROMO-XXXX'."
+        )
         String codePromo
 ) {}
